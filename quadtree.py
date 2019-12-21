340d762953ee249af40fffbbbabeb19a151e2f3c
Anna Krusova <anicka.krusova@gmail.com>
2019-12-21 23:22:14 +0100

    aktualizace a přidání programu split

--- quadtree.py
+++ quadtree.py
@@ -5,17 +5,20 @@ def add_cluster_id(feature, cluster_id):
 
 
 def get_breakpoints(features):
-    coordinates = []
-    for coordinate in features['features']:
-        xy = coordinate['geometry']['coordinates']
-        coordinates.append(xy)
-    coordinates.sort(key=lambda p: p[0])
-    x_min = coordinates[0][0]
-    x_max = coordinates[-1][0]
-    coordinates.sort(key=lambda p: p[1])
-    y_min = coordinates[0][1]
-    y_max = coordinates[-1][1]
-    return x_min, x_max, y_min, y_max
+    x_coordinates = []
+    y_coordinates = []
+    for coordinate in features:
+        x_axis = coordinate['geometry']['coordinates'][0]
+        y_axis = coordinate['geometry']['coordinates'][1]
+        x_coordinates.append(x_axis)
+        y_coordinates.append(y_axis)
+    x_min = min(x_coordinates)
+    x_max = max(x_coordinates)
+    y_min = min(y_coordinates)
+    y_max = max(y_coordinates)
+    x_mid = (x_min + x_max) / 2
+    y_mid = (y_min + y_max) / 2
+    return x_min, x_max, y_min, y_max, x_mid, y_mid
 
 
 def split_features(features, x_mid, y_mid):
@@ -23,21 +26,21 @@ def split_features(features, x_mid, y_mid):
     features2 = []
     features3 = []
     features4 = []
-    for point in features['features']:
+    for point in features:
         coordinates = point['geometry']['coordinates']
         x = coordinates[0]
         y = coordinates[1]
         if x <= x_mid and y > y_mid:
-            add_cluster_id(point, 1)
+            add_cluster_id(point, '1')
             features1.append(point)
         elif x > x_mid and y > y_mid:
-            add_cluster_id(point, 2)
+            add_cluster_id(point, '2')
             features2.append(point)
         elif x <= x_mid and y <= y_mid:
-            add_cluster_id(point, 3)
+            add_cluster_id(point, '3')
             features3.append(point)
         elif x > x_mid and y <= y_mid:
-            add_cluster_id(point, 4)
+            add_cluster_id(point, '4')
             features4.append(point)
         # body ležící na osách jsou přidány buď do 1. nebo 4. kvadrantu
         elif x == x_mid and y > y_mid or x < x_mid and y == y_mid or x == x_mid and y == y_mid:
@@ -49,7 +52,7 @@ def split_features(features, x_mid, y_mid):
     return features1, features2, features3, features4
 
 
-def quad_tree(features, output_list, x_mid, y_mid, x_qlen, y_qlen, q=0):
+def quad_tree(features, output_list, x_mid, y_mid, x_qlen, y_qlen, quad=0):
 
     #  input: listy features 1-4
     if len(features) < 50:
@@ -57,22 +60,22 @@ def quad_tree(features, output_list, x_mid, y_mid, x_qlen, y_qlen, q=0):
             output_list.append(point)
         return output_list
 
-    if q == 1:
+    if quad == 1:
         x_mid = x_mid - x_qlen
         y_mid = y_mid + y_qlen
-    if q == 2:
+    if quad == 2:
         x_mid = x_mid + x_qlen
         y_mid = y_mid + y_qlen
-    if q == 3:
+    if quad == 3:
         x_mid = x_mid - x_qlen
         y_mid = y_mid - y_qlen
-    if q == 4:
+    if quad == 4:
         x_mid = x_mid + x_qlen
         y_mid = y_mid - y_qlen
 
     features1, features2, features3, features4 = split_features(features, x_mid, y_mid)
 
-    quad_tree(features1, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, q=1)
-    quad_tree(features2, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, q=2)
-    quad_tree(features3, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, q=3)
-    quad_tree(features4, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, q=4)
+    quad_tree(features1, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, quad=1)
+    quad_tree(features2, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, quad=2)
+    quad_tree(features3, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, quad=3)
+    quad_tree(features4, output_list, x_mid, y_mid, x_qlen/2, y_qlen/2, quad=4)
---
+++ split.py
@@ -0,0 +1,25 @@
+import json
+import quadtree as q
+
+# načtení vstupních dat a uložení do proměnné
+with open("input.geojson", "r", encoding="utf-8") as f:
+    data = json.load(f)
+features = data["features"]
+
+x_min, x_max, y_min, y_max, x_mid, y_mid = q.get_breakpoints(features)
+
+
+x_qlen = abs(x_min + x_max) / 2
+y_qlen = abs(y_min + y_max) / 2
+
+# nejdřív
+quad = 0
+
+# seznam pro výsledné body
+output_list = []
+
+output_list = q.quad_tree(features, output_list, x_qlen, y_qlen, x_mid, y_mid, quad)
+
+gj_structure = {'type': 'FeatureCollection', 'features': output_list}
+with open("output.geojson", "w", encoding="utf-8") as f:
+    json.dump(gj_structure, f, indent=2, ensure_ascii=False)
